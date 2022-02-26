a59c6c0b49b7ac1c19130a8a85c03a2457c707d6
Zathef <khalilbtrovao@outlook.com>
2022-02-25 23:09:33 -0300

    test2

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -15,4 +15,16 @@ public class PrimeiroPrograma {
 class Livros {
 	private String nome;
 	private String npag;
+	public String getNpag() {
+		return npag;
+	}
+	public void setNpag(String npag) {
+		this.npag = npag;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
